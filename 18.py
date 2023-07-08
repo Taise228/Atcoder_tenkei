4e6578dfe147cb47e80237b8c7bca2cadec44cde
Taise228 <yamamototaisei534@gmail.com>
2023-07-08 11:14:39 +0900

    flake8

--- 18.py
+++ 18.py
@@ -9,11 +9,6 @@ def main():
     for _ in range(Q):
         E = int(input())
         angle = 2 * np.pi * E / T
-        y = (-1) * (L / 2) * np.sin(angle) - Y
-        z = (-1) * (L / 2) * np.cos(angle) + (L / 2)
-        vec_a = np.array([-X, y, z])
-        vec_b = np.array([-X, y, 0])
-        # theta = np.rad2deg(np.arccos(np.dot(vec_a, vec_b) / (np.linalg.norm(vec_a) * np.linalg.norm(vec_b))))
         denominator = np.sqrt(1 + ((L / 2) * (1 - np.cos(angle))) ** 2 / (X ** 2 + (Y + L / 2 * np.sin(angle)) ** 2))
         theta = np.rad2deg(np.arccos(1 / denominator))
         print(theta)
